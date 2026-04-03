17934d63920596885d6ad7e1fcfd6e34f64027d9
nikosild <nikosild@gmail.com>
2026-04-03 21:07:49 +0300

    Add files via upload

--- core/tracker.lua
+++ core/tracker.lua
@@ -88,8 +88,8 @@ end
 
 function tracker.record_damage(zone, dmg, now)
     local s = tracker.get_session(zone)
-    s.total_damage = s.total_damage + (dmg * 150)
-    table.insert(s.damage_log, { t = now, dmg = dmg * 150 })
+    s.total_damage = s.total_damage + (dmg * 15)
+    table.insert(s.damage_log, { t = now, dmg = dmg * 15 })
 end
 
 function tracker.record_kill(zone)
